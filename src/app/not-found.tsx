f4366b0e70d1145b86998611395541e7e3268f9b
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-02 11:12:52 +0400

    [main] Added custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default async function NotFound() {
+  return (
+    <main className="min-h-[100svh] grid place-content-center p-3">
+      <p className="text-center">404 | This page could not be found.</p>
+    </main>
+  );
+}
