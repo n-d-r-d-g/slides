90f708f3fa2526cbb04b1875c59594594e40d05d
n-d-r-d-g <n.d.r.d.g@hotmail.com>
2024-08-15 19:33:08 +0400

    [main] Updated content

--- src/app/Front-End-Coders-Mauritius/2024-08-24/Contributing-to-an-Open-Source-Community/slides/OpenSourceCharacteristicsSlide.tsx
+++ src/app/Front-End-Coders-Mauritius/2024-08-24/Contributing-to-an-Open-Source-Community/slides/OpenSourceCharacteristicsSlide.tsx
@@ -12,36 +12,52 @@ export function OpenSourceCharacteristicsSlide() {
     <Slide>
       <Heading className="leading-tight">{"Characteristics"}</Heading>
       <Grid gridTemplateColumns="1fr 1fr" gridColumnGap={16}>
-        <UnorderedList fontSize={40} className="flex flex-col leading-snug">
-          <ListItem>
-            <span className="font-black underline">{"code is public:"}</span>
-            {" transparency v/s risk of being copied"}
-          </ListItem>
-          <ListItem>
-            <span className="font-black underline">
-              {"contributions are public:"}
+        <UnorderedList fontSize={30} className="flex flex-col leading-snug">
+          <ListItem className="flex flex-col">
+            <span className="font-black underline text-[40px]">
+              {"Code is public:"}
             </span>
-            {" increased visibility v/s mistakes are also visible"}
+            <span>{"transparency v/s risk of being copied"}</span>
           </ListItem>
-          <ListItem>
-            <span className="font-black underline">{"networking:"}</span>
-            {" new business v/s social awkwardness"}
+          <ListItem className="flex flex-col">
+            <span className="font-black underline text-[40px]">
+              {"Contributions are public:"}
+            </span>
+            <span>{"increased visibility v/s mistakes are also visible"}</span>
+          </ListItem>
+          <ListItem className="flex flex-col">
+            <span className="font-black underline text-[40px]">
+              {"Self-improvement:"}
+            </span>
+            <span>
+              {
+                "improve technical & soft skills, patience v/s possible in private communities"
+              }
+            </span>
           </ListItem>
         </UnorderedList>
-        <UnorderedList fontSize={40} className="flex flex-col leading-snug">
-          <ListItem>
-            <span className="font-black underline">{"self-improvement:"}</span>
-            {
-              " improve technical & soft skills, patience v/s possible in private communities"
-            }
-          </ListItem>
-          <ListItem>
-            <span className="font-black underline">
-              {"contributions are generally not paid:"}
+        <UnorderedList fontSize={30} className="flex flex-col leading-snug">
+          <ListItem className="flex flex-col">
+            <span className="font-black underline text-[40px]">
+              {"Networking:"}
             </span>
-            {
-              " help keep open source communities open v/s no incentive (priorities)"
-            }
+            <span>{"new business v/s social awkwardness"}</span>
+          </ListItem>
+          <ListItem className="flex flex-col">
+            <span className="font-black underline text-[40px]">
+              {"Contributions are generally not paid:"}
+            </span>
+            <span>
+              {
+                "help keep open source communities open v/s no incentive (priorities)"
+              }
+            </span>
+          </ListItem>
+          <ListItem className="flex flex-col">
+            <span className="font-black underline text-[40px]">
+              {"More leverage:"}
+            </span>
+            <span>{"higher pay v/s more tricky questions"}</span>
           </ListItem>
         </UnorderedList>
       </Grid>
