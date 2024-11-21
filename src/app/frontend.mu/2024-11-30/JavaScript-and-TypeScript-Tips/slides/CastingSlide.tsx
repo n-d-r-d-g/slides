d6106ad1cd76b95d80dc9a4f43cd54c18e5084a4
n-d-r-d-g <96367205+n-d-r-d-g@users.noreply.github.com>
2024-11-21 05:37:04 +0400

    [main] Added CastingSlide

---
+++ src/app/frontend.mu/2024-11-30/JavaScript-and-TypeScript-Tips/slides/CastingSlide.tsx
@@ -0,0 +1,44 @@
+import { Code } from "@/app/components/Code";
+import { Heading } from "@/app/components/Heading";
+import { Notes, Slide } from "spectacle";
+
+export function CastingSlide() {
+  return (
+    <Slide className="w-full h-full max-w-full max-h-full flex flex-col [&>div]:w-full [&>div]:h-full">
+      <Heading>{"Casting"}</Heading>
+      <div className="max-w-full h-[32rem] mx-auto">
+        <Code lang="typescript">
+          {`type Config = { [key: string]: string | number; }` +
+            `\nconst config = {` +
+            `\n\twidth: '100px',` +
+            `\n} as Config;` +
+            `\nconfig.depth = '40px';` +
+            `\nconfig.width; //  Typed as string | number` +
+            `\nconfig.width = 20;` +
+            `\nconfig.width; //  typed as number` +
+            `\nconfig.isActive = true; // Type 'boolean' is not assignable to type 'string | number'`}
+        </Code>
+      </div>
+      <Notes>
+        <ul className="list-disc list-inside flex flex-col gap-2">
+          <li>
+            {
+              "We can add key-value pairs to config during and after initialization without modifying anything else (no type/interface needs modification) IF THE TYPE/INTERFACE IS LOOSE."
+            }
+          </li>
+          <li>
+            {
+              "config is bound to type Config, i.e. config's properties have to respect their respective types."
+            }
+          </li>
+          <li>
+            {
+              "Each property's type is string | number if not modified after initialization. If it is, it's either a string or number based on its value."
+            }
+          </li>
+          <li>{"Key autocomplete doesn't work."}</li>
+        </ul>
+      </Notes>
+    </Slide>
+  );
+}
